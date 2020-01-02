d5338261e0217992ea1023598b6a77ecb20091cf
Daniel Mocanu <danielmocanu3@gmail.com>
2020-01-02 15:39:11 -0500

    Updates

---
+++ precache-manifest.97a5afae8d866ca52141b026475d1f37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb8a55ab45e435a74f95928731ff6e4",
+    "url": "/userface/index.html"
+  },
+  {
+    "revision": "543d46304d5e58ddbd5d",
+    "url": "/userface/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3fcc0f4055297df0472a",
+    "url": "/userface/static/css/main.0fdef6d0.chunk.css"
+  },
+  {
+    "revision": "543d46304d5e58ddbd5d",
+    "url": "/userface/static/js/2.92b08654.chunk.js"
+  },
+  {
+    "revision": "3fcc0f4055297df0472a",
+    "url": "/userface/static/js/main.c9e71cd3.chunk.js"
+  },
+  {
+    "revision": "cf2e1c6517c9658d5b67",
+    "url": "/userface/static/js/runtime-main.d4b1083b.js"
+  }
+]);
